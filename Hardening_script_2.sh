e488428eaa46cc78d3549fb6cda21f5800f9383b
julcarol <julissa02cornejo@gmail.com>
2025-04-16 21:19:33 -0400

    Update Hardening_script_2.sh

--- Hardening_script_2.sh
+++ Hardening_script_2.sh
@@ -1,44 +1,44 @@
 
 
-\#\!/bin/bash
+#!/bin/bash
 
-\# Variable for the report output file, choose a **NEW** output file name  
+# Variable for the report output file, choose a **NEW** output file name  
 REPORT\_FILE="hardening\_script\_2.sh"
 
-\# Output the sshd configuration file  
+# Output the sshd configuration file  
 echo "Gathering details from sshd configuration file"  
-\# Placeholder for command to get the sshd configuration file
+# Placeholder for command to get the sshd configuration file
 
 echo "sshd configuration file:$(cat /etc/ssh/sshd\_config)" \>\> $REPORT\_FILE  
 printf "\\n" \>\> $REPORT\_FILE
 
-\# Update packages and services  
+# Update packages and services  
 Echo “Updating packages and services”
 
-\# Placeholder for command to update packages
+# Placeholder for command to update packages
 
 sudo apt update \-y
 
-\# Placeholder for command to upgrade packages
+# Placeholder for command to upgrade packages
 
 sudo apt upgrade \-y 
 
 echo "Packages have been updated and upgraded" \>\> $REPORT\_FILE  
 printf "\\n" \>\> $REPORT\_FILE
 
-\# Placeholder for command to list all installed packages
+# Placeholder for command to list all installed packages
 
 echo "Installed Packages:$(sudo apt list \--installed)" \>\> $REPORT\_FILE  
 printf "\\n" \>\> $REPORT\_FILE
 
 echo “Printing out logging configuration data”
 
-\# Placeholder for command to display logging data
+# Placeholder for command to display logging data
 
 echo "journald.conf file data: $(cat /etc/systemd/journald.conf)" \>\> $REPORT\_FILE  
 printf "\\n" \>\> $REPORT\_FILE
 
-\# Placeholder for command to display logrotate data
+# Placeholder for command to display logrotate data
 
 echo "logrotate.conf file data:$(cat /etc/logrotate.conf)" \>\> $REPORT\_FILE  
 printf "\\n" \>\> $REPORT\_FILE
